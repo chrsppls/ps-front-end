9a9a1ed798b42c7da30f8b90b599989d8f83090f
chrs.ppls <chrs_ppls@yahoo.com>
2020-03-17 10:37:38 -0400

    5th Commit: Addition of the account menu, pre-event listeners.

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   activePraise();
+  openAccountMenu('.account-menu');
+  hideOnClickOutside('.account-menu');
 
 });
-
-
 
 function activePraise() {
   $('.praise__box').click(function(event) {
@@ -12,4 +12,34 @@ function activePraise() {
     $(event.target).toggleClass('active-praise');
     $(event.target.querySelector('.praise__box .praise__box--icon')).toggleClass('active-praise__icon');
   })
+}
+
+function openAccountMenu(selector) {
+  var userNav = $('.header__user-nav');
+
+
+
+  $(userNav).click(function(event) {
+
+    $(selector).toggleClass('display-none');
+  })
+
+
+}
+
+function hideOnClickOutside(selector) {
+
+  const outsideClickListener = (event) => {
+    $target = $(event.target);
+    if (!$target.closest(selector).length && $(selector).is(':visible')) {
+      $(selector).toggleClass('display-none');
+      removeClickListener();
+    }
+  }
+
+  const removeClickListener = () => {
+    document.removeEventListener('click', outsideClickListener)
+  }
+
+  document.addEventListener('click', outsideClickListener)
 }
